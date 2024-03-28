2113f47d9200661eda55b6be8e06661e4274b2be
Mauricio Rojas <mrojasb2000@gmail.com>
2024-03-28 18:01:31 -0300

    refactor: character type and list component

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl: 'list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent { }
+export class ListComponent {
+  public characterList: Character[] = [{
+    name: "Trunks",
+    power: 10,
+  }]
+ }
